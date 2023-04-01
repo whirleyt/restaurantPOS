e3051dc2e8de262f91bc3184ada14f4500649f0a
Tara Whirley <whirleyt@outlook.com>
2023-04-01 13:45:10 -0400

    Log in functionality complete

--- server.py
+++ server.py
@@ -1,12 +1,11 @@
 
 """
-Columbia's COMS W4111.001 Introduction to Databases
+Columbia's COMS W4111.001 Introduction to Databases RestaurantPOS
 """
 import os
-# accessible as a variable in index.html:
 from sqlalchemy import *
 from sqlalchemy.pool import NullPool
-from flask import Flask, request, render_template, g, redirect, Response, url_for
+from flask import Flask, request, render_template, g, redirect, Response, url_for, session
 
 tmpl_dir = os.path.join(os.path.dirname(
     os.path.abspath(__file__)), 'templates')
@@ -16,39 +15,12 @@ DATABASE_USERNAME = "tjw2154"
 DATABASE_PASSWRD = "6104"
 DATABASE_HOST = "34.28.53.86"
 DATABASEURI = f"postgresql://{DATABASE_USERNAME}:{DATABASE_PASSWRD}@{DATABASE_HOST}/project1"
+app.secret_key = '6104'
 
-#
-# This line creates a database engine that knows how to connect to the URI above.
-#
 engine = create_engine(DATABASEURI)
-
-#
-# Example of running queries in your database
-# Note that this will probably not work if you already have a table named 'test' in your database, containing meaningful data. This is only an example showing you how to run queries in your database using SQLAlchemy.
-#
-with engine.connect() as conn:
-    create_table_command = """
-	CREATE TABLE IF NOT EXISTS test (
-		id serial,
-		name text
-	)
-	"""
-    res = conn.execute(text(create_table_command))
-    insert_table_command = """INSERT INTO test(name) VALUES ('grace hopper'), ('alan turing'), ('ada lovelace')"""
-    res = conn.execute(text(insert_table_command))
-    # you need to commit for create, insert, update queries to reflect
-    conn.commit()
-
 
 @app.before_request
 def before_request():
-    """
-    This function is run at the beginning of every web request
-    (every time you enter an address in the web browser).
-    We use it to setup a database connection that can be used throughout the request.
-
-    The variable g is globally accessible.
-    """
     try:
         g.conn = engine.connect()
     except:
@@ -57,35 +29,17 @@ def before_request():
         traceback.print_exc()
         g.conn = None
 
-
 @app.teardown_request
 def teardown_request(exception):
-    """
-    At the end of the web request, this makes sure to close the database connection.
-    If you don't, the database could run out of memory!
-    """
     try:
         g.conn.close()
     except Exception as e:
         pass
 
-
-#
-# @app.route is a decorator around index() that means:
-#   run index() whenever the user tries to access the "/" path using a GET request
-#
-# If you wanted the user to go to, for example, localhost:8111/foobar/ with POST or GET then you could use:
-#
-#       @app.route("/foobar/", methods=["POST", "GET"])
-#
-# PROTIP: (the trailing / in the path is important)
-#
-# see for routing: https://flask.palletsprojects.com/en/1.1.x/quickstart/#routing
-# see for decorators: http://simeonfranklin.com/blog/2012/jul/1/python-decorators-in-12-steps/
-#
-
 @app.route('/login/', methods=['POST', 'GET'])
 def login():
+    if 'loggedin' in session and session['loggedin']== True:
+        return redirect(url_for('index'))
     if request.method == "POST":
       username=request.form['username']
       password=request.form['password']
@@ -101,160 +55,110 @@ def login():
       g.conn.commit()
       if(user[0][2] != int(password)):
         error = dict(error=True)
+        session['loggedin']= False
         return render_template("user_login.html", **error)
-        # return redirect(url_for('login', error=True))
       else:
+        session['loggedin']= True
+        session['employeeID'] = user[0][2]
+        session['firstName'] = user[0][0]
+        session['lastName'] = user[0][1]
         return redirect(url_for('index'))
     error = dict(error=False)
     return render_template("user_login.html", **error)
 
-
+@app.route('/logout/')
+def logout():
+   session.pop('loggedin', None)
+   session.pop('employeeID', None)
+   session.pop('firstName', None)
+   session.pop('lastName', None)
+   return redirect(url_for('login'))
 
 @app.route('/')
 def default():
     return redirect(url_for('login'))
 
-
-@app.route('/tables', methods=['POST', 'GET'])
+@app.route('/tables/', methods=['POST', 'GET'])
 def index():
-    """
-    request is a special object that Flask provides to access web request information:
+    # print(request.args)
+    if 'loggedin' in session and session['loggedin']== True:
+        select_query = "SELECT t.tableID, t.section_ID, t.guestCount FROM Tables t;"
+        cursor = g.conn.execute(text(select_query))
+        tables = []
+        for result in cursor:
+            tables.append(result)
+        cursor.close()
+        context = dict(data=tables)
+        return render_template("index.html", **context)
+    else:
+        return redirect(url_for('login'))
 
-    request.method:   "GET" or "POST"
-    request.form:     if the browser submitted a form, this contains the data in the form
-    request.args:     dictionary of URL arguments, e.g., {a:1, b:2} for http://localhost?a=1&b=2
-
-    See its API: https://flask.palletsprojects.com/en/1.1.x/api/#incoming-request-data
-    """
-
-    # DEBUG: this is debugging code to see what request looks like
-    print(request.args)
-
-    # example of a database query
-
-    select_query = "SELECT t.tableID, t.section_ID, t.guestCount FROM Tables t;"
-    cursor = g.conn.execute(text(select_query))
-    tables = []
-    for result in cursor:
-        tables.append(result)
-    cursor.close()
-
-
-    #
-    # Flask uses Jinja templates, which is an extension to HTML where you can
-    # pass data to a template and dynamically generate HTML based on the data
-    # (you can think of it as simple PHP)
-    # documentation: https://realpython.com/primer-on-jinja-templating/
-    #
-    # You can see an example template in templates/index.html
-    #
-    # context are the variables that are passed to the template.
-    # for example, "data" key in the context variable defined below will be
-    # accessible as a variable in index.html:
-    #
-    context = dict(data=tables)
-
-    #
-    # render_template looks in the templates/ folder for files.
-    # for example, the below file reads template/index.html
-    #
-    return render_template("index.html", **context)
-
-#
-# This is an example of a different path.  You can see it at:
-#
-#     localhost:8111/another
-#
-# Notice that the function name is another() rather than index()
-# The functions for each app.route need to have different names
-#
-@app.route('/order/<id>')
+@app.route('/order/<id>/')
 def order(id):
-    select_order_query = "SELECT mi.name, mi.price, oi.table_ID, oi.orderID FROM MenuItem mi JOIN OrderItems oi ON oi.menu_item_ID=mi.menuItemID AND oi.table_ID="+id+";"
-    cursor1 = g.conn.execute(text(select_order_query))
-    order = []
-    for result in cursor1:
-        order.append(result)
-    cursor1.close()
-    select_total_query = "SELECT SUM(mi.price) AS OrderTotal FROM MenuItem mi JOIN OrderItems oi ON oi.menu_item_ID=mi.menuItemID AND oi.table_ID="+id+";"
-    cursor2 = g.conn.execute(text(select_total_query))
-    order_total= [];
-    for result in cursor2:
-        order_total.append(result)
-    cursor2.close()
-    url=[]
-    url.append(id)
-    order_context = dict(data=order)
-    total_context = dict(total=order_total)
-    url_id= dict(urlid=url);
-    return render_template("order.html", **order_context, **total_context, **url_id)
+    if 'loggedin' in session and session['loggedin']== True:
+        select_order_query = "SELECT mi.name, mi.price, oi.table_ID, oi.orderID FROM MenuItem mi JOIN OrderItems oi ON oi.menu_item_ID=mi.menuItemID AND oi.table_ID="+id+";"
+        cursor1 = g.conn.execute(text(select_order_query))
+        order = []
+        for result in cursor1:
+            order.append(result)
+        cursor1.close()
+        select_total_query = "SELECT SUM(mi.price) AS OrderTotal FROM MenuItem mi JOIN OrderItems oi ON oi.menu_item_ID=mi.menuItemID AND oi.table_ID="+id+";"
+        cursor2 = g.conn.execute(text(select_total_query))
+        order_total= [];
+        for result in cursor2:
+            order_total.append(result)
+        cursor2.close()
+        url=[]
+        url.append(id)
+        order_context = dict(data=order)
+        total_context = dict(total=order_total)
+        url_id= dict(urlid=url)
+        return render_template("order.html", **order_context, **total_context, **url_id)
+    else:
+        return redirect(url_for('login'))
 
-@app.route('/order/<id>/<menuItemTypeId>')
+@app.route('/order/<id>/<menuItemTypeId>/')
 def menuItems(id, menuItemTypeId):
-    select_menu_item_query = "SELECT mi.name, mi.menuItemID FROM MenuItem mi WHERE mi.type_ID="+menuItemTypeId+";"
-    cursor1 = g.conn.execute(text(select_menu_item_query))
-    menuItems = []
-    for result in cursor1:
-      menuItems.append(result)
-    cursor1.close()
-    menu_item_context = dict(menuItem=menuItems)
-    select_order_query = "SELECT mi.name, mi.price, oi.table_ID, oi.orderID FROM MenuItem mi JOIN OrderItems oi ON oi.menu_item_ID=mi.menuItemID AND oi.table_ID="+id+";"
-    cursor2 = g.conn.execute(text(select_order_query))
-    order = []
-    for result in cursor2:
-        order.append(result)
-    cursor2.close()
-    select_total_query = "SELECT SUM(mi.price) AS OrderTotal FROM MenuItem mi JOIN OrderItems oi ON oi.menu_item_ID=mi.menuItemID AND oi.table_ID="+id+";"
-    cursor3 = g.conn.execute(text(select_total_query))
-    order_total= [];
-    for result in cursor3:
-        order_total.append(result)
-    url=[]
-    url.append(id)
-    url_id= dict(urlid=url);
-    cursor3.close()
-    order_context = dict(data=order)
-    total_context = dict(total=order_total)
-    return render_template("order.html", **menu_item_context, **order_context, **total_context, **url_id)
+    if 'loggedin' in session and session['loggedin']== True:
+        select_menu_item_query = "SELECT mi.name, mi.menuItemID FROM MenuItem mi WHERE mi.type_ID="+menuItemTypeId+";"
+        cursor1 = g.conn.execute(text(select_menu_item_query))
+        menuItems = []
+        for result in cursor1:
+            menuItems.append(result)
+        cursor1.close()
+        menu_item_context = dict(menuItem=menuItems)
+        select_order_query = "SELECT mi.name, mi.price, oi.table_ID, oi.orderID FROM MenuItem mi JOIN OrderItems oi ON oi.menu_item_ID=mi.menuItemID AND oi.table_ID="+id+";"
+        cursor2 = g.conn.execute(text(select_order_query))
+        order = []
+        for result in cursor2:
+            order.append(result)
+        cursor2.close()
+        select_total_query = "SELECT SUM(mi.price) AS OrderTotal FROM MenuItem mi JOIN OrderItems oi ON oi.menu_item_ID=mi.menuItemID AND oi.table_ID="+id+";"
+        cursor3 = g.conn.execute(text(select_total_query))
+        order_total= [];
+        for result in cursor3:
+            order_total.append(result)
+        url=[]
+        url.append(id)
+        url_id= dict(urlid=url);
+        cursor3.close()
+        order_context = dict(data=order)
+        total_context = dict(total=order_total)
+        return render_template("order.html", **menu_item_context, **order_context, **total_context, **url_id)
+    else:
+        return redirect(url_for('login'))
 
-@app.route('/another')
-def another():
-    return render_template("another.html")
-
-
-# Example of adding new data to the database
-@app.route('/order/<id>', methods=['POST'])
+@app.route('/order/<id>/', methods=['POST'])
 def add(id):
-    # accessing form inputs from user
     table_ID=request.form['table_ID']
     menu_item_ID=request.form['menu_item_ID']
     params = {}
     params["table_ID"]= table_ID
     params["menu_item_ID"]= menu_item_ID
-    params["name"]= 'Tara';
+    params["name"]= 'Tara'
     g.conn.execute(text('INSERT INTO OrderItems (menu_item_ID, table_ID, createdBy) VALUES (:menu_item_ID, :table_ID, :name)'), params)
-    # g.conn.execute(text('INSERT INTO test(name) VALUES (:new_name)'), params)
     g.conn.commit()
     return redirect(url_for('order', id=table_ID))
-
-
-
-
-    # name = request.form['name']
-
-    # # passing params in for each variable into query
-    # params = {}
-    # params["new_name"] = name
-    g.conn.execute(text('INSERT INTO test(name) VALUES (:new_name)'), params)
-    g.conn.commit()
-    return redirect('/')
-
-
-# @app.route('/login')
-# def login():
-#     abort(401)
-#     this_is_never_executed()
-
 
 if __name__ == "__main__":
     import click
@@ -265,20 +169,7 @@ if __name__ == "__main__":
     @click.argument('HOST', default='0.0.0.0')
     @click.argument('PORT', default=8101, type=int)
     def run(debug, threaded, host, port):
-        """
-        This function handles command line parameters.
-        Run the server using:
-
-                python server.py
-
-        Show the help text using:
-
-                python server.py --help
-
-        """
-
         HOST, PORT = host, port
         print("running on %s:%d" % (HOST, PORT))
         app.run(host=HOST, port=PORT, debug=debug, threaded=threaded)
-
 run()
